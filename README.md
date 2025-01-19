d8a60d53cdc52746c6c184f676d03649b3ff0cf2
Aidan Christie <aidanchristie2232@gmail.com>
2025-01-19 05:47:11 +0000

    1.0.1 initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Basics
+Mainly a plugin to allow commands normally used on a server and some useful features hence "basic" stuff.
+
+Commands:
+-
+- Heal
+- Feed
+- Flight
+- Gamemode
+- Settings
+- Help
+- Friend
+
+Feature:
+- 
+- Local friends system
+- Currency system
+- Basic needs for server
+- Settings for players to toggle
+- Help menu for players
--- src/main/java/me/shini9000/basics/Basics.java
+++ src/main/java/me/shini9000/basics/Basics.java
@@ -1,17 +1,25 @@
 package me.shini9000.basics;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public final class Basics extends JavaPlugin {
+    private BukkitTask task;
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Basics enabling!");
+
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Basics getInstance() {
+        return getPlugin(Basics.class);
     }
 }
